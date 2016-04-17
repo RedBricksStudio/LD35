366e82d0f9839c5568e64fae5737b9f09a80f6dc
NoisyBass <laura.dcs4@gmail.com>
2016-04-18 01:09:34 +0200

    Shapeshifting and player attack implemented

--- Game/Scenes/Player/Player.gd
+++ Game/Scenes/Player/Player.gd
@@ -2,13 +2,14 @@ extends KinematicBody2D
 
 var speed = 250
 var direction = Vector2(0, 0)
-var shapes = ["robot", "guard", "attendee"]
+# var shapes = ["robot", "guard", "attendee"]
+var shapes = ["robot"]
 var current_shape = shapes[0]
 var attacking = false
 var talking = false
 var shapeshifting = false
-export var talk_threshold = 10000
-export var attack_threshold = 100
+export var talk_threshold = 5000
+export var attack_threshold = 5000
 onready var sprite = get_node("Sprite")
 onready var robot_texture = preload("res://Textures/icon.tex")
 onready var guard_texture = preload("res://Textures/enemy.tex")
@@ -28,6 +29,7 @@ func _fixed_process(delta):
 		attacking = true
 		direction = Vector2(0, 0)
 		get_node("PlayerAnimations").play("Attack")
+		attack()
 #	elif Input.is_action_pressed("prev_shape"):
 #		shapeshifting = true
 #		direction = Vector2(0, 0)
@@ -54,7 +56,7 @@ func get_shape():
 
 func next_shape():
 	current_shape = shapes[(shapes.find(current_shape) + 1 ) % shapes.size()]
-	print(current_shape)
+	get_node("Sprite").set_texture(texture_dict[current_shape])
 
 func talk():
 	var talkable_npcs = get_tree().get_nodes_in_group("talkable")
@@ -74,13 +76,13 @@ func attack():
 	var attackable_npcs = get_tree().get_nodes_in_group("npcs")
 	if attackable_npcs.empty():
 		return
-	var closest_distance = 9999
-	var closest
+	var closest_distance = get_pos().distance_squared_to(attackable_npcs[0].get_pos())
+	var closest = attackable_npcs[0]
 	for npc in attackable_npcs:
 		if get_pos().distance_squared_to(npc.get_pos()) < closest_distance:
 			closest_distance = get_pos().distance_squared_to(npc.get_pos())
 			closest = npc
-	
+			
 	if closest_distance < attack_threshold:
 		var npc_shape = closest._being_attacked()
 		if shapes.find(npc_shape) == -1:
